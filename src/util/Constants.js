eb74719362a6a30cbe87bbb22c98dde61e85feb0
Otoniel Moreira Duarte <otonielduarte2@gmail.com>
2020-07-15 10:44:04 -0300

    Update Constants.js
    Remove keys

--- src/util/Constants.js
+++ src/util/Constants.js
@@ -1,5 +1,5 @@
-export const PUBLIC_KEY = '2a3a746ec7a8387029256cc89dbc0df8';
+export const PUBLIC_KEY = '';
 
-export const PRIVATE_KEY = 'c7880c7239120779c7276b19e1e9b977fed17e42';
+export const PRIVATE_KEY = '';
 
 export const URL_API = 'http://gateway.marvel.com/v1/public/';
